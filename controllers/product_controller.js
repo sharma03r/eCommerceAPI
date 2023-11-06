a0c32aa3ef20ce950b2492c152b08a58610ae70c
Rahul Sharma <sharmarahul8848@gmail.com>
2023-11-06 23:50:33 +0530

    create, update and delete product feature added

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -16,3 +16,65 @@ module.exports.index = function (req, res) {
       }
     });
 };
+
+module.exports.create = function (req, res) {
+  Product.create({
+    id: req.body.id,
+    name: req.body.name,
+    quantity: req.body.quantity,
+  })
+    .then((product) => {
+      if (true) {
+        return res.status(200).json({
+          data: {
+            product: product,
+          },
+          message: "Product added successfully",
+        });
+      }
+    })
+    .catch((err) => {
+      if (err) {
+        return res.status(500).json({
+          message: "Internal Server Error",
+        });
+      }
+    });
+};
+module.exports.delete = function (req, res) {
+  let product = Product.deleteOne({ id: req.params.id });
+  if (product) {
+    console.log(product);
+    product
+      .deleteOne()
+      .then(() => {
+        return res.status(200).json({
+          message: "Product deleted successfully",
+        });
+      })
+      .catch((err) => {
+        return res.status(500).json({
+          message: "Internal Server Error",
+        });
+      });
+  }
+};
+
+module.exports.updateQuantity = function (req, res) {
+  Product.findByIdAndUpdate(
+    { id: req.params.id },
+    {
+      $pull: { quantity: req.query.number },
+    }
+  )
+    .then(() => {
+      return res.status(200).json({
+        message: "Product has been updated",
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        message: "Internal Server Error",
+      });
+    });
+};
--- routes/products.js
+++ routes/products.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const productController = require("../controllers/product_controller");
 
 router.get("/", productController.index);
+router.post("/create", productController.create);
+router.delete("/:id", productController.delete);
+router.post("/:id/update_quantity/?number=", productController.updateQuantity);
 
 module.exports = router;
